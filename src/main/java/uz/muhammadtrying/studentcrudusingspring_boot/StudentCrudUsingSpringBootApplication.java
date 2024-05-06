6c73f5dc31db13164718ec3fcdfff0c43cf09b38
Muhammad <muhammadtrying@gmail.com>
2024-05-06 10:25:45 +0500

    Initial commit

---
+++ src/main/java/uz/muhammadtrying/studentcrudusingspring_boot/StudentCrudUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package uz.muhammadtrying.studentcrudusingspring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/muhammadtrying/studentcrudusingspring_boot/StudentCrudUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.muhammadtrying.studentcrudusingspring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
